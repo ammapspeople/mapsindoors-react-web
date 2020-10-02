82c41715d7acce60bfde4a755853d01c5016a662
Anders Mogensen <am@mapspeople.com>
2020-10-02 10:02:23 +0200

    Implement basic app shell without any functionality.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { useRef, useEffect } from 'react';
 import './App.css';
 
+
+function AppTitle(props) {
+  return <header className="app-title">
+      <h1>{props.title}</h1>
+  </header>;
+}
+
+function NavButton() {
+  return <div className="app-nav">☰</div>
+}
+
+
 function App() {
   const mapElementReference = useRef(null); // We need a reference to the mi-map in order to add event listeners.
 
@@ -12,9 +24,11 @@ function App() {
 
   return (
     <div className="App">
-      <mi-card>
+      <AppTitle title="Welcome to MapsIndoors"></AppTitle>
+      <NavButton></NavButton>
+      <div className="map">
         <mi-map ref={mapElementReference} zoom="18" position-control="TOP_CENTER" mi-api-key="demo" gm-api-key="AIzaSyBNhmxW2OntKAVs7hjxmAjFscioPcfWZSc" floor-selector="RIGHT_CENTER"></mi-map>
-      </mi-card>
+      </div>
     </div>
   );
 }
