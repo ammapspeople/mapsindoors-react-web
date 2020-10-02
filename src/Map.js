97b3d98c2cfa80776a3ab85fb1450b8e200281ba
Anders Mogensen <am@mapspeople.com>
2020-10-02 10:50:04 +0200

    Pop up card when clicking a location.

--- src/Map.js
+++ src/Map.js
@@ -1,15 +1,24 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function Map() {
+  let [location, setLocation] = useState({});
+
   const mapElementReference = useRef(null); // We need a reference to the mi-map in order to add event listeners.
 
   useEffect(() => { // The function passed to useEffect will run after the render is committed to the screen.
       mapElementReference.current.addEventListener('mapsIndoorsReady', () => {
         mapElementReference.current.panTo({lat: 38.8979504120527, lng: -77.03665279788513});
       });
+
+      mapElementReference.current.addEventListener('locationClicked', location => {
+        setLocation(location.detail);
+    });
   });
   return <div className="map">
     <mi-map ref={mapElementReference} zoom="18" position-control="TOP_CENTER" mi-api-key="demo" gm-api-key="AIzaSyBNhmxW2OntKAVs7hjxmAjFscioPcfWZSc" floor-selector="RIGHT_CENTER"></mi-map>
+    {location.id &&
+        <mi-card class="map-card">Location</mi-card>
+    }
   </div>
 }
 
